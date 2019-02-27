044198af590523dbc05712ca0f11ac2f8df8169d
esmeralva <esmeralva930@gmail.com>
2019-02-27 09:38:11 -0500

    clock

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     function displayTime(){
       var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var seconds = currentTime.getSeconds();
+
+      var clockDiv = document.getElementByID('clock');
+
+      clockDiv.innerText = hours + ": " + minutes + ": " + seconds;
     }
+
+    // displayTime();
+
+    setInterval(displayTime, 1000);
 
 });
